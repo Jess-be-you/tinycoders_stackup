d4acfa4d52869582aedd99ae6080ad4cd4a6007b
Jess-be-you <jesmipj001@gmail.com>
2023-11-11 19:36:44 +0530

    updated

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,23 +1,15 @@
 import express from 'express';
-import { singupUser,loginUser } from '../controller/user-controller.js';
+import { sigupUser,loginUser } from '../controller/user-controller.js';
 import { createPost, deletePost, getAllPosts,getPost, updatePost } from '../controller/post-controller.js';
 import { uploadImage,getImage } from '../controller/image-controller.js';
-// import { newComment, getComments, deleteComment } from '../controller/comment-controller.js';
-// import { loginUser, singupUser, logoutUser } from '../controller/user-controller.js';
-import { authenticateToken, createNewToken } from '../controller/jwt-controller.js';
+import { authenticateToken } from '../controller/jwt-controller.js';
 import upload from '../utils/upload.js';
 const router = express.Router();
 
 router.post('/login', loginUser);
-router.post('/signup', singupUser);
-// router.post('/logout', logoutUser);
-
-// router.post('/token', createNewToken);
+router.post('/signup', signupUser);
 
 router.post('/create', authenticateToken, createPost);
-// router.put('/update/:id', authenticateToken, updatePost);
-// router.delete('/delete/:id', authenticateToken, deletePost);
-
 router.get('/posts', authenticateToken, getAllPosts);
 router.get('/post/:id', authenticateToken, getPost);
 router.put('/update/:id',authenticateToken,updatePost);
@@ -25,10 +17,6 @@ router.delete('/delete/:id',authenticateToken,deletePost)
 
 router.post('/file/upload', upload.single('file'), uploadImage);
 router.get('/file/:filename', getImage);
-
-// router.post('/comment/new', authenticateToken, newComment);
-// router.get('/comments/:id', authenticateToken, getComments);
-// router.delete('/comment/delete/:id', authenticateToken, deleteComment);
 
 
 export default router;
